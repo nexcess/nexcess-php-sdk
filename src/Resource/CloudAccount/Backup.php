aa3bc7478b6fa2e6019d2a5eb328c0488c03e8c9
Cal Evans <cevans@nexcess.net>
2018-10-17 14:37:38 -0400

    Backup Entity

---
+++ src/Resource/CloudAccount/Backup.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package Nexcess-SDK
+ * @license https://opensource.org/licenses/MIT
+ * @copyright 2018 Nexcess.net, LLC
+ */
+
+declare(strict_types  = 1);
+
+namespace Nexcess\Sdk\Resource\CloudAccount;
+
+use Nexcess\Sdk\ {
+  Resource\App\Entity as App,
+  Resource\Model,
+  Util\Util
+};
+
+/**
+ * Backup Entity
+ */
+class Backup extends Model {
+
+  /** {@inheritDoc} */
+  public const MODULE_NAME = 'CloudAccount';
+
+  /** {@inheritDoc} */
+  protected const _PROPERTY_ALIASES = [];
+
+  /** {@inheritDoc} */
+  protected const _PROPERTY_COLLAPSED = [];
+
+  /** {@inheritDoc} */
+  protected const _PROPERTY_MODELS = [];
+
+  /** {@inheritDoc} */
+  protected const _PROPERTY_NAMES = [];
+
+  /** {@inheritDoc} */
+  protected const _READONLY_NAMES = [
+    'filepath',
+    'filename',
+    'filesize',
+    'filesize_bytes',
+    'type',
+    'domain_options',
+    'filedate',
+    'complete'
+  ];
+
+  public function download(string $target_location) : bool {
+
+  }
+
+  public function getStatus() : string {
+
+  }
+
+  public function delete() : bool {
+
+  }
+
+  protected static function createFromPayload(string $payload) : Backup {
+
+  }
+
+}
