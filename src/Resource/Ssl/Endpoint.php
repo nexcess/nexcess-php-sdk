f1c5408394116dd1e28a953e080612787a7385e9
Cal Evans <cevans@nexcess.net>
2018-11-14 09:09:27 -0500

    importCertificate()

--- src/Resource/Ssl/Endpoint.php
+++ src/Resource/Ssl/Endpoint.php
@@ -29,6 +29,11 @@ class Endpoint extends ReadableEndpoint {
   /** {@inheritDoc} */
   protected const _URI = 'ssl-cert';
 
+  /**
+   * Retrieve a certificate by it's service_id
+   *
+   * @param int $service_id a valid service_id for a certificate
+   */
   public function retrieveByServiceId(int $service_id) : Entity {
     $filter = ['filter' => ['service_id' => $service_id]];
     $response = $this->_client->request(
@@ -37,4 +42,29 @@ class Endpoint extends ReadableEndpoint {
     );
     return $this->getModel()->sync(Util::decodeResponse($response)[0]);
   }
+
+  /**
+   * Import an existing certificate
+   *
+   * @param Entity $entity Cloud Server model
+   * @param string $key the key to the crt
+   * @param string $crt the crt
+   * @param string $chain The chain certificate
+   *
+   * @return Entity
+   * @throws GuzzleHttp\Exception\ClientException If request fails
+   */
+  public function importCertificate(
+    string $key,
+    string $crt,
+    string $chain = ''
+  ) : Entity {
+    $response = $this->_client->post(
+      self::_URI,
+      ['json' => ['key' => $key, 'crt' => $crt, 'chain' => $chain]]
+    );
+
+    return $this->getModel()->sync(Util::decodeResponse($response));
+  }
+
 }
