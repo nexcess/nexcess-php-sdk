3cdcd09071596d63f31f12c50100ea14a1692fdc
Cal Evans <cevans@nexcess.net>
2018-11-19 15:00:47 -0500

    fleshed out getParamsProvier

--- tests/Resource/Ssl/EndpointTest.php
+++ tests/Resource/Ssl/EndpointTest.php
@@ -28,10 +28,10 @@ class EndpointTest extends EndpointTestCase {
   protected const _RESOURCE_PATH = __DIR__ . '/resources';
 
   /** {@inheritDoc} */
-  protected _RESOURCE_GET = 'GET-%2Fssl-cert%2F.json'
+  protected const _RESOURCE_GET = 'GET-%2Fssl-cert%2F.json';
 
   /** {@inheritDoc} */
-  protected _RESOURCE_IMPORT = 'POST-%2Fssl-cert%2F.json'
+  protected const _RESOURCE_IMPORT = 'POST-%2Fssl-cert%2F.json';
 
   /** {@inheritDoc} */
   protected const _RESOURCE_INSTANCES = [];
@@ -53,22 +53,109 @@ class EndpointTest extends EndpointTestCase {
    */
   public function getParamsProvider() : array {
     return [
-      'retrieveByServiceId' => [
-        'service_id' => [
+      [
+        'retrieveByServiceId',
+        [
+          'service_id' => [
+              Util::TYPE_INT,
+              true,
+              'service_id (integer): Required. ' .
+                Language::get('resource.Ssl.retrieveByServiceId.service_id')
+            ]
+        ]
+      ],
+      ['importCertificate',
+        [
+          'key' => [
+            Util::TYPE_STRING,
+            true,
+              'key (string): Required. ' .
+                Language::get('resource.Ssl.importCertificate.key')
+          ],
+          'crt' => [
+            Util::TYPE_STRING,
+            true,
+              'crt (string): Required. ' .
+                Language::get('resource.Ssl.importCertificate.crt')
+          ],
+          'chain' => [
+            Util::TYPE_STRING,
+            true,
+              'chain (string): Required. ' .
+                Language::get('resource.Ssl.importCertificate.chain')
+          ]
+
+        ]
+      ],
+      ['createCertificateFromCsr',
+        [
+          'csr' => [
+            Util::TYPE_STRING,
+            true,
+            'csr (string): Required. ' .
+            Language::get('resource.Ssl.createCertificateFromCsr.csr')
+          ],
+          'key' => [
+            Util::TYPE_STRING,
+            true,
+            'key (string): Required. ' .
+            Language::get('resource.Ssl.createCertificateFromCsr.key')
+          ],
+          'months' => [
             Util::TYPE_INT,
             true,
-            'service_id (integer): Required. ' .
-              Language::get('resource.Ssl.retrieveByServiceId.service_id')
+            'months (integer): Required. ' .
+            Language::get('resource.Ssl.createCertificateFromCsr.months')
+          ],
+          'package_id' => [
+            Util::TYPE_INT,
+            true,
+            'package_id (integer): Required. ' .
+            Language::get('resource.Ssl.createCertificateFromCsr.package_id')
+          ],
+          'approver_emails' => [
+            Util::TYPE_ARRAY,
+            true,
+            'approver_emails (array): Required. ' .
+            Language::get('resource.Ssl.createCertificateFromCsr.approver_emails')
+          ],
+        ]
+      ],
+      ['createCertificate',
+        [
+          'domain' => [
+            Util::TYPE_STRING,
+            true,
+            'domain (string): Required. ' .
+            Language::get('resource.Ssl.createCertificate.domain')
+          ],
+          'distinguished_name' => [
+            Util::TYPE_ARRAY,
+            true,
+            'distinguished_name (array): Required. ' .
+            Language::get('resource.Ssl.createCertificate.distinguished_name')
+          ],
+          'months' => [
+            Util::TYPE_INT,
+            true,
+            'months (integer): Required. ' .
+            Language::get('resource.Ssl.createCertificate.months')
+          ],
+          'package_id' => [
+            Util::TYPE_INT,
+            true,
+            'package_id (integer): Required. ' .
+            Language::get('resource.Ssl.createCertificate.package_id')
+          ],
+          'approver_emails' => [
+            Util::TYPE_ARRAY,
+            true,
+            'approver_emails (array): Required. ' .
+            Language::get('resource.Ssl.createCertificate.approver_emails')
           ]
-      ],
-      'importCertificate' => [
-      ],
-      'testCreateCertificateFromCsr' => [
-      ],
-      'testCreateCertificateByCSR' => [
-      ],
-      'testCreateCertificateByData' => [
-      ],
+
+        ]
+      ]
     ];
   }
 
@@ -76,35 +163,35 @@ class EndpointTest extends EndpointTestCase {
    * @covers Ssl::retrieveByServiceId
    */
    public function testRetrieveByServiceId() {
-
+     $this->markTestIncomplete('This test has not been implemented yet.');
    }
 
   /**
    * @covers Ssl::importCertificate
    */
    public function testImportCertificate() {
-
+     $this->markTestIncomplete('This test has not been implemented yet.');
    }
 
   /**
    * @covers Ssl::createCertificateFromCsr
    */
    public function testCreateCertificateFromCsr() {
-
+     $this->markTestIncomplete('This test has not been implemented yet.');
    }
 
   /**
    * @covers Ssl::createCertificate
    */
    public function testCreateCertificateByCSR() {
-
+     $this->markTestIncomplete('This test has not been implemented yet.');
    }
 
   /**
    * @covers Ssl::createCertificate
    */
    public function testCreateCertificateByData() {
-
+     $this->markTestIncomplete('This test has not been implemented yet.');
    }
 
 }
