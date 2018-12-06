b8d0a556bc010efb712837e1cc2fdbae06873fd6
Cal Evans <cevans@nexcess.net>
2018-12-06 08:59:51 -0500

    testCreateFromCsr()

--- tests/Resource/Ssl/SslTest.php
+++ tests/Resource/Ssl/SslTest.php
@@ -39,9 +39,15 @@ class SslTest extends ModelTestCase {
 
   protected const _RESOURCE_GET_BY_SERVICE_ID = 'ssl-by-service-id.json';
 
-  protected const _RESOURCE_CSR = 'csr_2.txt';
+  protected const _RESOURCE_CRT = 'csr.txt';
 
-  protected const _RESOURCE_KEY = 'key_2.txt';
+  protected const _RESOURCE_KEY = 'key.txt';
+
+  protected const _RESOURCE_CHAIN = 'chain.txt';
+
+  protected const _RESOURCE_CSR_2 = 'csr_2.txt';
+
+  protected const _RESOURCE_KEY_2 = 'key_2.txt';
 
   /**
    * @covers Ssl::create
@@ -96,7 +102,42 @@ class SslTest extends ModelTestCase {
           'package_id' => 179,
           'domain' => 'example.com',
           'approver_email' => ['example.com' => ['admin@example.com']],
+          'key' => $this->_getResource(self::_RESOURCE_KEY_2)
+        ]
+      ]
+    );
+
+    $response = $ssl->createFromCsr(
+          $this->_getResource(self::_RESOURCE_CSR_2)
+        );
+
+    $this->assertEquals(Ssl::class, get_class($response));
+    $this->assertEquals(123, $response->get('id'));
+    $this->assertEquals('example.com', $response->get('common_name'));
+  }
+
+  public function testImport() {
+    $endpoint = $this->createMock(Endpoint::class);
+    $endpoint->expects($this->once())
+      ->method('import')
+      ->willReturn(
+        (new Ssl())->sync(
+          $this->_getResource(self::_RESOURCE_GET_BY_SERVICE_ID)[0]
+        )
+      );
+      /*
+        $this->get('key'),
+        $this->get('crt'),
+        $this->get('chain')
+      */
+
+    $ssl = Ssl::__set_state(
+      [
+        '_endpoint' => $endpoint,
+        '_values' => [
           'key' => $this->_getResource(self::_RESOURCE_KEY)
+          'crt' => $this->_getResource(self::_RESOURCE_CRT)
+          'chain' => $this->_getResource(self::_RESOURCE_CHAIN)
         ]
       ]
     );
@@ -108,9 +149,6 @@ class SslTest extends ModelTestCase {
     $this->assertEquals(Ssl::class, get_class($response));
     $this->assertEquals(123, $response->get('id'));
     $this->assertEquals('example.com', $response->get('common_name'));
-  }
-
-  public function testImport() {
   }
 
 
